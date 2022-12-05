86ba342623cd5a5197e8897f329c8ce475a93317
HollywoodOKC <T_Lane11@yahoo.com>
2022-12-05 17:25:56 -0600

    2nd

--- menu.js
+++ menu.js
@@ -4,14 +4,14 @@
 /*
     In this file, you'll be writing code that
     deals with food objects, arrays of objects
-    and filtering those arrays. 
+    and filtering those arrays.
 */
 
 
 //////////////////PROBLEM 1////////////////////
-/*  
+/*
     Create an object called `pizza` that has 6
-    properties: 
+    properties:
         - name (string)
         - price (number)
         - category (string)
@@ -30,19 +30,27 @@
     'kids'
 */
 
-//CODE HERE
+const pizza = {
+    name: `Meat Lovers`,
+    price: 20,
+    category: `Meat Lover`,
+    populartity: 100,
+    rating: 100,
+    tags: [`kids`, `Large`, `This-crust`]
+}
 
 
 
 //////////////////PROBLEM 2////////////////////
-/* 
+/*
     Let's print a few values from our pizza object.
 
-    First, log the popularity of pizza. 
+    First, log the popularity of pizza.
     Use dot notation to access the value.
 */
 
-//CODE HERE
+pizza.populartity
+console.log(pizza.populartity)
 
 
 /*
@@ -52,31 +60,30 @@
     get the value.
 */
 
-//CODE HERE
+console.log(pizza.tags[1]);
 
 
 /*
     Third, destructure the price off of the
     pizza object.
-    
+
     Print the value of your new price variable.
 */
 
-//CODE HERE
-
+let price = pizza.price;
 
 /*
     Fourth, and last, destructure the category
     property.
 
-    Print the value of your category variable. 
+    Print the value of your category variable.
 */
 
-//CODE HERE
+let category = pizza.category;
 
 
 //////////////////PROBLEM 3////////////////////
-/* 
+/*
     Create an array with about 5 objects in it.
     The objects should mimic the `pizza` object.
     Call the array `foodArr`.
@@ -87,12 +94,19 @@
     data in some functions that you'll write.
 */
 
-//CODE HERE
+let foodArr = {
+    name: `Supreme`,
+    price: 15,
+    category: `Loaded`,
+    populartity: 75,
+    rating: 90,
+    tags: [`Meats`, `Veggies`, `Cheesy`]
+}
 
 
 
 //////////////////PROBLEM 4////////////////////
-/* 
+/*
     Let's filter the food objects according
     to their tags.
 
@@ -105,29 +119,29 @@
 
 //CODE HERE
 
-// const filteredFood = foodArr.filter(/* CALLBACK HERE */)
+const filteredFood = foodArr.filter(/* CALLBACK HERE */)
 
 
 
 //////////////////PROBLEM 5////////////////////
-/* 
+/*
     Now let's write a function that's a little
     more flexible than just filtering for one
-    value. We want to be able to filter for 
-    food that has above a certain rating, 
+    value. We want to be able to filter for
+    food that has above a certain rating,
     below a certain price, or any other combo.
 
     Write a function called `filterByProperty`
-    that takes in three arguments: `property`, 
-    `number`, and `type. 
+    that takes in three arguments: `property`,
+    `number`, and `type.
 
     The property will be a string (rating,
     popularity, or price)
 
     The number will be the number that you want
-    to compare against 
+    to compare against
 
-    The type should be 'above' or 'below' to 
+    The type should be 'above' or 'below' to
     indicate whether you want to get foods with
     values that are above or below the given number
     for the given property
@@ -137,14 +151,14 @@
 
     Use the filter method to filter the foodArr
 
-        In the callback, check if the `type` is `above`, 
+        In the callback, check if the `type` is `above`,
         if it is, return objects whose value for the given
         property is greater than the `number` passed in
 
         If the type is `below`, return objects whose
-        value for the given property is less than the 
+        value for the given property is less than the
         `number` passed in
-    
+
     Return the filtered array from the entire function
 */
 
